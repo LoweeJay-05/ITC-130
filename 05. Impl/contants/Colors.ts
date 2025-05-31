6fbf60dd29813b8325368e812834111a6b89fd36
Coli127 <magwangaynicoleen127@gmail.com>
2025-05-31 16:52:39 +0800

    Create Colors.ts

---
+++ 05. Impl/contants/Colors.ts
@@ -0,0 +1,26 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const tintColorLight = '#93E9BE';
+const tintColorDark = '#fff';
+
+export const Colors = {
+  light: {
+    text: '#A9A9A9',
+    background: '#fff',
+    tint: tintColorLight,
+    icon: '#A9A9A9',
+    tabIconDefault: '#A9A9A9',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#A9A9A9',
+    background: '#A9A9A9',
+    tint: tintColorDark,
+    icon: '#A9A9A9',
+    tabIconDefault: '#A9A9A9',
+    tabIconSelected: tintColorDark,
+  },
+};
